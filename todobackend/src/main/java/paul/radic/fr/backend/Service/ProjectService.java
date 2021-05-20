0baa792e59d77c97bee89031ab0da2cec352067d
Paul Radic <paul.radic@epita.fr>
2021-05-20 17:30:38 +0200

    [Backend] init service

--- todobackend/src/main/java/paul/radic/fr/backend/Service/ProjectService.java
+++ todobackend/src/main/java/paul/radic/fr/backend/Service/ProjectService.java
@@ -1,2 +1,40 @@
-package paul.radic.fr.backend.Service;public class ProjectService {
+package paul.radic.fr.backend.Service;
+
+import paul.radic.fr.backend.Entity.Project;
+import paul.radic.fr.backend.Entity.UserProject;
+import paul.radic.fr.backend.Entity.Users;
+import paul.radic.fr.backend.Repository.ProjectRepository;
+import paul.radic.fr.backend.Repository.UserProjectRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@ApplicationScoped
+public class ProjectService
+{
+    @Inject
+    ProjectRepository projectRepository;
+    @Inject
+    UserProjectRepository userProjectRepository;
+
+
+    public void addProject(Project project)
+    {
+        projectRepository.persist(project);
+    }
+    public boolean deleteProject(long id)
+    {
+        return projectRepository.delete("id", id) != 0;
+    }
+    public List<Project> findProjects(String uuid)
+    {
+        return userProjectRepository.find("user_uuid", uuid).stream().map(UserProject::getProject).collect(Collectors.toList());
+    }
+    public List<Users> getUsers(Project project)
+    {
+        return userProjectRepository.find("project_id", project.id).stream().map(UserProject::getUser).collect(Collectors.toList());
+    }
 }
--- todobackend/src/main/java/paul/radic/fr/backend/Service/UsersService.java
+++ todobackend/src/main/java/paul/radic/fr/backend/Service/UsersService.java
@@ -1,2 +1,26 @@
-package paul.radic.fr.backend.Service;public class UsersService {
+package paul.radic.fr.backend.Service;
+
+import paul.radic.fr.backend.Entity.Users;
+import paul.radic.fr.backend.Repository.UserRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ApplicationScoped
+public class UsersService {
+    @Inject
+    UserRepository userRepository;
+    public List<Users> getAll()
+    {
+        return userRepository.listAll();
+    }
+    public void addUser(Users user)
+    {
+        userRepository.persist(user);
+    }
+    public void deleteUser(String uuid)
+    {
+        userRepository.delete("uuid",uuid);
+    }
 }
